b3018dd0e54ae135f6120672bca395ea7713b68c
LynaBatoche <152402326+LynaBatoche@users.noreply.github.com>
2025-04-24 20:36:06 +0100

    Update 3_3_allChercheurs.sql

--- 3_3_allChercheurs.sql
+++ 3_3_allChercheurs.sql
@@ -1,1 +1,10 @@
+-- Use LabDatabase
+USE LabDatabase;
+GO
+
+-- Display all Chercheur (NumCh, NomCh) of laboratoire with CodeLab = '0001'
+SELECT NumCh, NomCh
+FROM Chercheur
+WHERE CodeLab = '0001';
+GO
 
